e946acd98439e160b164da09a87a79be63ff2d96
P-mandevillei <chenglin.l@wustl.edu>
2026-04-22 11:03:17 -0500

    modules; might break

---
+++ ExactKernels.py
@@ -0,0 +1,263 @@
+import numpy as np
+import math
+from scipy.optimize import minimize
+try:
+  import numba as nb
+  from numba import cuda
+except:
+  %pip install numba-cuda
+  import numba as nb
+  from numba import cuda
+try:
+  import cupy as cp
+except:
+  %pip install cupy-cuda12x
+
+from Constants import *
+from Forces import *
+
+# -------------- Compare: Sequential ---------------------
+@nb.njit
+def calc_force_sequential(
+  forces: np.ndarray, # global forces array (output)
+  positions: np.ndarray, # global positions array (input)
+  const_params: np.ndarray
+):
+  forces.fill(0)
+  params = const_params
+  n_particles = positions.shape[1]
+  for i in range(n_particles-1):
+    for j in range(i+1, n_particles):
+      distance = 0
+      for k in range(SPACE_N_DIM):
+        dx = minimum_image_1d(positions[k, i] - positions[k, j], params[BOX_SIZE_IDX])
+        distance += dx**2
+      distance = math.sqrt(distance)
+      magnitude = calc_force(distance, params, j - i)
+      for k in range(SPACE_N_DIM):
+        f = magnitude * minimum_image_1d(positions[k, i] - positions[k, j], params[BOX_SIZE_IDX]) / max(distance, DIV_BY_ZERO_GUARD)
+        forces[k, i] += f
+        forces[k, j] -= f
+
+# ------------ Method 0: Segmented Traversal ------------------
+@cuda.jit
+def calc_force_segmented(
+  forces: cp.ndarray, # global forces array (output)
+  positions: cp.ndarray, # global positions array (input)
+  const_params: cp.ndarray
+):
+  dtype = nb.float64
+  params = cuda.const.array_like(const_params)
+
+  idx = cuda.grid(1)
+  receiver_position = cuda.local.array(shape = SPACE_N_DIM, dtype = dtype)
+  if idx < positions.shape[1]:
+    for dim in range(SPACE_N_DIM):
+      receiver_position[dim] = positions[dim, idx]
+
+  src_positions = cuda.shared.array(shape = (SEGMENT, 3), dtype = dtype)
+  force = cuda.local.array(shape = SPACE_N_DIM, dtype = dtype)
+  for i in range(SPACE_N_DIM):
+    force[i] = 0
+
+  for start in range(0, positions.shape[1], SEGMENT):
+    sweep = min(SEGMENT, positions.shape[1] - start)
+    # cooperate to load src positions
+    if cuda.threadIdx.x < sweep:
+      for dim in range(SPACE_N_DIM):
+        src_positions[cuda.threadIdx.x, dim] = positions[dim, start + cuda.threadIdx.x]
+    cuda.syncthreads()
+
+    # calculate
+    if idx < positions.shape[1]:
+      for i in range(sweep):
+        distance = 0
+        for dim in range(SPACE_N_DIM):
+          distance += minimum_image_1d(receiver_position[dim] - src_positions[i, dim], params[BOX_SIZE_IDX])**2
+        distance = math.sqrt(distance)
+        magnitude = calc_force(
+          distance,
+          params,
+          idx - (start + i)
+        )
+        for dim in range(SPACE_N_DIM):
+          force[dim] += magnitude * minimum_image_1d(receiver_position[dim] - src_positions[i, dim], params[BOX_SIZE_IDX]) / max(distance, DIV_BY_ZERO_GUARD)
+    cuda.syncthreads()
+  if idx < positions.shape[1]:
+    for dim in range(SPACE_N_DIM):
+      forces[dim, idx] = force[dim] 
+
+# ------------ Method 1: Force Matrix ------------------
+@cuda.jit
+def calc_force_matrix(
+  forces: cp.ndarray, # global forces array (output)
+  positions: cp.ndarray, # global positions array (input)
+  tile_n: int,
+  const_params: cp.ndarray
+):
+  dtype = nb.float64
+  params = cuda.const.array_like(const_params)
+
+  # heavy index calculation once per block
+  
+  block_idx = cuda.shared.array(shape = 2, dtype = nb.int32) # row, col
+  if cuda.threadIdx.x == 0:
+    row = math.ceil(tile_n - 0.5 - math.sqrt((2*tile_n+1)*(2*tile_n+1) - 8*(cuda.blockIdx.x+1))/2)
+    col = cuda.blockIdx.x + row - (2*tile_n-row+1)*row//2
+    block_idx[0] = row * FORCE_MAT_TILE_DIM
+    block_idx[1] = col * FORCE_MAT_TILE_DIM
+  cuda.syncthreads()
+
+  # load source positions to shared memory
+  sweep = min(FORCE_MAT_TILE_DIM, forces.shape[1]-block_idx[0])
+  src_positions = cuda.shared.array(shape = (FORCE_MAT_TILE_DIM, 3), dtype = dtype)
+  if cuda.threadIdx.x < sweep:
+    for dim in range(SPACE_N_DIM):
+      src_positions[cuda.threadIdx.x, dim] = positions[dim, block_idx[0]+cuda.threadIdx.x]
+  cuda.syncthreads()
+
+  receiver_idx = block_idx[1] + cuda.threadIdx.x
+
+  # ---------------------- diagonal --------------------------------
+  if block_idx[0] == block_idx[1]:
+    if receiver_idx < forces.shape[1]: # this guarantees threadIdx < sweep since the force matrix is a square matrix
+      # load receiver positions
+      receiver_position = cuda.local.array(shape = 3, dtype = dtype)
+      for dim in range(SPACE_N_DIM):
+        receiver_position[dim] = src_positions[cuda.threadIdx.x, dim]
+      
+      col_sum = cuda.local.array(shape = SPACE_N_DIM, dtype = dtype)
+      for i in range(SPACE_N_DIM):
+        col_sum[i] = 0
+      for i in range(1, sweep): # start from 1 to skip calculating forces by itself, avoid division by 0 distance
+        # juxtapose src indices
+        # when not diagonal, minimize shared memory contention
+        src_idx = (i + cuda.threadIdx.x) % sweep
+        distance = 0
+        for dim in range(SPACE_N_DIM):
+          distance += minimum_image_1d(receiver_position[dim] - src_positions[src_idx, dim], params[BOX_SIZE_IDX])**2
+        distance = math.sqrt(distance)
+        magnitude = calc_force(
+          distance,
+          params,
+          receiver_idx - (block_idx[0] + src_idx)
+        )
+        for dim in range(SPACE_N_DIM):
+          col_sum[dim] += magnitude * minimum_image_1d(receiver_position[dim] - src_positions[src_idx, dim], params[BOX_SIZE_IDX]) / max(distance, DIV_BY_ZERO_GUARD)
+      for dim in range(SPACE_N_DIM):
+        cuda.atomic.add(forces, (dim, receiver_idx), col_sum[dim])
+  
+  # ---------------------- non-diagonal --------------------------------
+  else:
+    row_sums = cuda.shared.array(shape = (FORCE_MAT_TILE_DIM, 3), dtype = dtype) # sweep must be equal to tile dim when it's not a diagonal block
+    for i in range(SPACE_N_DIM):
+      row_sums[cuda.threadIdx.x, i] = 0
+    cuda.syncthreads()
+
+    if receiver_idx < forces.shape[1]:
+      col_sum = cuda.local.array(shape = SPACE_N_DIM, dtype = dtype)
+      for i in range(SPACE_N_DIM):
+        col_sum[i] = 0
+      receiver_position = cuda.local.array(shape = SPACE_N_DIM, dtype = dtype)
+      for dim in range(SPACE_N_DIM):
+        receiver_position[dim] = positions[dim, receiver_idx]
+      
+      for i in range(sweep):
+        src_idx = (i + cuda.threadIdx.x) % sweep
+        distance = 0
+        for dim in range(SPACE_N_DIM):
+          distance += minimum_image_1d(receiver_position[dim] - src_positions[src_idx, dim], params[BOX_SIZE_IDX])**2
+        distance = math.sqrt(distance)
+        magnitude = calc_force(
+          distance,
+          params,
+          receiver_idx - (block_idx[0] + src_idx)
+        )
+        for dim in range(SPACE_N_DIM):
+          f = magnitude * minimum_image_1d(receiver_position[dim] - src_positions[src_idx, dim], params[BOX_SIZE_IDX]) / max(distance, DIV_BY_ZERO_GUARD)
+          col_sum[dim] += f
+          cuda.atomic.add(row_sums, (src_idx, dim), -f) # no race condition when FORCE_MAT_TILE_DIM <= WARP_SIZE
+      for dim in range(SPACE_N_DIM):
+        cuda.atomic.add(forces, (dim, receiver_idx), col_sum[dim])
+
+    cuda.syncthreads()
+
+    if cuda.threadIdx.x < sweep:
+      for dim in range(SPACE_N_DIM):
+        cuda.atomic.add(forces, (dim, cuda.threadIdx.x+block_idx[0]), row_sums[cuda.threadIdx.x, dim])
+
+# ---------- Method 2: Explicit Force Matrix ----------
+@cuda.jit
+def calc_force_matrix_explicit(
+  forces_mat: cp.ndarray, # global forces matrix array (1d) (output)
+  positions: cp.ndarray, # global positions array (input)
+  const_params: cp.ndarray
+):
+  dtype = nb.float64
+  params = cuda.const.array_like(const_params)
+  idx = cuda.grid(1)
+  n = positions.shape[1] - 1 # -1 since we don't need the diagonal entries
+  if idx >= n*(n+1)//2:
+    return
+
+  row = int(math.ceil(n - 0.5 - math.sqrt((2*n+1)*(2*n+1) - 8*(idx+1))/2))
+  col = idx + row - (2*n-row+1)*row//2 + 1 # +1 since we don't need the diagonal entries
+  
+  src_positions = cuda.local.array(SPACE_N_DIM, dtype = dtype)
+  receiver_positions = cuda.local.array(SPACE_N_DIM, dtype = dtype)
+  for dim in range(SPACE_N_DIM):
+    src_positions[dim] = positions[dim, row]
+    receiver_positions[dim] = positions[dim, col]
+  distance = 0
+  for dim in range(SPACE_N_DIM):
+    distance += minimum_image_1d(receiver_positions[dim] - src_positions[dim], params[BOX_SIZE_IDX])**2
+  distance = math.sqrt(distance)
+  magnitude = calc_force(
+    distance,
+    params,
+    col - row
+  )
+  for dim in range(SPACE_N_DIM):
+    forces_mat[dim, idx] = magnitude * minimum_image_1d(receiver_positions[dim] - src_positions[dim], params[BOX_SIZE_IDX]) / max(distance, DIV_BY_ZERO_GUARD)
+
+@cuda.jit
+def force_mat_sum(
+  forces_mat: cp.ndarray, # upper diagonal matrix stored as 1d array
+  forces: cp.ndarray # output array
+):
+  dtype = nb.float64
+  coarsen_factor = (forces.shape[1] + FORCE_MAT_REDUC_BLOCK_DIM - 1) // FORCE_MAT_REDUC_BLOCK_DIM
+  n = forces.shape[1] - 1
+  results = cuda.shared.array(shape = (FORCE_MAT_REDUC_BLOCK_DIM, SPACE_N_DIM), dtype = dtype)
+
+  result = cuda.local.array(shape = SPACE_N_DIM, dtype = dtype)
+  for dim in range(SPACE_N_DIM):
+    result[dim] = 0
+  for idx in range(cuda.threadIdx.x*coarsen_factor, min(forces.shape[1], (cuda.threadIdx.x+1)*coarsen_factor)):
+    col = cuda.blockIdx.x
+    row = idx
+    if row == col:
+      continue
+    coeff = 1
+    if row > col:
+      row, col = col, row
+      coeff = -1
+    col -= 1
+    mat_idx = (2*n-row+1)*row//2 + col - row
+    for dim in range(SPACE_N_DIM):
+      result[dim] += coeff*forces_mat[dim, mat_idx]
+  for dim in range(SPACE_N_DIM):
+    results[cuda.threadIdx.x, dim] = result[dim]
+  cuda.syncthreads()
+
+  block = FORCE_MAT_REDUC_BLOCK_DIM // 2
+  while block > 0:
+    if cuda.threadIdx.x < block:
+      for dim in range(SPACE_N_DIM):
+        results[cuda.threadIdx.x, dim] += results[cuda.threadIdx.x + block, dim]
+    block //= 2
+    cuda.syncthreads()
+  
+  if cuda.threadIdx.x == 0:
+    for dim in range(SPACE_N_DIM):
+      forces[dim, cuda.blockIdx.x] = results[0, dim]
---
+++ Forces.py
@@ -0,0 +1,135 @@
+import numpy as np
+import math
+from scipy.optimize import minimize
+try:
+  import numba as nb
+  from numba import cuda
+except:
+  %pip install numba-cuda
+  import numba as nb
+  from numba import cuda
+try:
+  import cupy as cp
+except:
+  %pip install cupy-cuda12x
+
+from Constants import *
+
+# -------------------------------- Initialize ----------------------------------------
+@nb.njit
+def assign_position(positions, new_positions, idx):
+    positions[0, idx] = new_positions[0]
+    positions[1, idx] = new_positions[1]
+    positions[2, idx] = new_positions[2]
+
+@nb.njit
+def initialize_chain_numba(
+    n_particles: int,
+    box_size: float,
+    r0: float,
+    rng: np.random._generator.Generator,
+    dtype=np.float32
+) -> np.ndarray:
+    """
+    Randomly initialize atom positions by growing the chain
+    """
+    positions = np.zeros((3, n_particles), dtype=dtype)
+
+    current_position = np.array([box_size / 2, box_size / 2, box_size / 2], dtype=dtype)
+    assign_position(positions, current_position, 0)
+
+    for i in range(1, n_particles):
+        direction = rng.normal(size = 3).astype(dtype)
+        norm = np.sqrt(direction[0]**2 + direction[1]**2 + direction[2]**2)
+        direction /= norm
+
+        next_position = current_position + r0 * direction
+        next_position = (next_position % box_size).astype(dtype) # pbc
+        assign_position(positions, next_position, i)
+        current_position = next_position
+
+    return positions
+
+def initialize_velocities_cupy(
+    n_particles: int,
+    target_temperature: float, 
+    mass: float,
+    rng: cp.random._generator_api.Generator,
+    kB=1.0,
+    dtype=cp.float32
+) -> cp.ndarray:
+    """
+    Initialize particle velocities by drawing from the Maxwell-Botzmann distribution at target temperature
+    """
+    sigma = cp.sqrt(kB * target_temperature / mass).astype(dtype)
+
+    velocities = rng.standard_normal(
+        size=(3, n_particles), 
+        dtype=dtype
+    )
+    velocities = velocities * sigma
+
+    # Remove center-of-mass velocity
+    velocities -= cp.mean(velocities, axis=-1).reshape(3, 1)
+
+    return velocities
+
+# -------------------------------- Define Forces --------------------------------
+@nb.njit
+def harmonic_force(r, k, r0):
+  return -k * (r-r0)
+
+well_coeff = 2**(1/6)
+@nb.njit
+def repulsive_lj_force(r, sigma, epsilon):
+  if r < well_coeff*sigma:
+    r = max(r, DIV_BY_ZERO_GUARD)
+    sixth_pow = (sigma/r) ** 6
+    return -24*epsilon/r * (sixth_pow - 2*(sixth_pow**2))
+  else:
+    return 0
+
+@nb.njit
+def attractive_lj_force(r, sigma, epsilon):
+  r = max(r, DIV_BY_ZERO_GUARD)
+  sixth_pow = (sigma/r) ** 6
+  return -24*epsilon/r * (sixth_pow - 2*(sixth_pow**2))
+
+@nb.njit
+def calc_force(distance, params, type):
+  if type == HARMONIC or type == -HARMONIC:
+     return harmonic_force(distance, params[K_IDX], params[R0_IDX])
+  if type == REPULSIVE or type == -REPULSIVE:
+     return repulsive_lj_force(distance, params[SIGMA_IDX], params[EPSILON_REPULSIVE_IDX])
+  if type >= ATTRACTIVE or type <= -ATTRACTIVE:
+     return attractive_lj_force(distance, params[SIGMA_IDX], params[EPSILON_ATTRACTIVE_IDX])
+  return 0
+
+@nb.njit
+def minimum_image_1d(dx, box_size):
+    return dx - box_size * math.floor((dx / box_size) + 0.5)
+
+# ---------------------------- Cutoff Kernels --------------------------------
+@nb.njit
+def calc_force_cutoff_sequential(
+  forces: np.ndarray, # global forces array (output)
+  positions: np.ndarray, # global positions array (input)
+  cutoff: float
+):
+  forces.fill(0)
+  params = const_params
+  n_particles = positions.shape[1]
+  for i in range(n_particles-1):
+    for j in range(i+1, n_particles):
+      distance = 0
+      for k in range(SPACE_N_DIM):
+        dx = minimum_image_1d(positions[k, i] - positions[k, j], params[BOX_SIZE_IDX])
+        distance += dx**2
+      distance = math.sqrt(distance)
+      if distance > cutoff:
+        continue
+      magnitude = calc_force(distance, params, j - i)
+      for k in range(SPACE_N_DIM):
+        f = magnitude * minimum_image_1d(positions[k, i] - positions[k, j], params[BOX_SIZE_IDX]) / max(distance, DIV_BY_ZERO_GUARD)
+        forces[k, i] += f
+        forces[k, j] -= f
